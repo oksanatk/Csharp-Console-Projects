e6faf1505b9b160ba81bc78293ec02acefbecafe
oksanatk <tkachoksn@gmail.com>
2024-08-24 16:12:16 -0400

    Add project files.

---
+++ ConsoleApp1 - first hello world/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+// uh oh. red squiggles. What is using System; ?
+//    do i need it to run a program in c#? or is a codecademy-specific quirk? 
+
+
+namespace myFirstOne
+{
+    class anotherFirst
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Squiggly jiggly Squidward vibes");
+            Console.WriteLine("Hey, didn't see you there. \nWhat brings you 'round this way?");
+            string welcomeInput = Console.ReadLine();
+            // where can I get an example of the usage of Console.Readline?
+            // Does it not print a string with it?
+
+            // how do I write a newline inside a string in C#? 
+            
+            Console.WriteLine(welcomeInput);
+             
+        }
+    }
+}
