848b5c53d5e4fd54f9d17282a19272d60166cd20
oksanatk <tkachoksn@gmail.com>
2024-12-27 12:02:46 -0500

    OOP practice

---
+++ SimpleProjects/OOP-Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+
+
+
+while (true)
+{
+    var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuChoices>()
+                    .Title("What do you want to do next?")
+                    .AddChoices(Enum.GetValues<MenuChoices>()));
+    switch (choice)
+    {
+        case MenuChoices.ViewBooks:
+            break;
+        case MenuChoices.AddBook:
+            break;
+        case MenuChoices.DeleteBook:
+            break;
+    }
+}
+
+
+
+enum MenuChoices
+{
+    ViewBooks,
+    AddBook,
+    DeleteBook
+}
---
+++ SimpleProjects/OOPPractice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+using Spectre.Console;
+
+internal class OOPPractice
+{
+	internal OOPPractice()
+	{
+		while (true)
+		{
+			var choice = AnsiConsole.Prompt(new SelectionPrompt<MenuOption>()
+									.Title("What do you want to do next?")
+									.AddChoices(Enum.GetValues<MenuOption>()));
+		}
+	}
+}
+
+enum MenuOption
+{
+	ViewBooks,
+	AddBook,
+	DeleteBook
+}
+	
+
